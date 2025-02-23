3c1d675f1d26b8e0dc4cf9dfb96389f5ca91d61e
Ashy's Coffee <ashleyleonidk@gmail.com>
2025-02-23 17:11:07 +0100

    Correction

--- managing-the-world-database/queries/data_types_indexes/queries_part1.sql
+++ managing-the-world-database/queries/data_types_indexes/queries_part1.sql
@@ -4,6 +4,10 @@ DESC country;
 DESC city;
 DESC countrylanguage;
 
+ALTER TABLE city ADD COLUMN is_population_large BOOLEAN GENERATED ALWAYS AS (population > 1000000) STORED;
+ALTER TABLE country ADD COLUMN region_code CHAR(3) DEFAULT 'NA';
+
+
 ALTER TABLE city ADD CONSTRAINT chk_population CHECK (population >= 0);
 ALTER TABLE country ADD CONSTRAINT unique_code UNIQUE (code);
 
